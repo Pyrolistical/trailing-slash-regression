856f24f4dcdb6090d858b031cfceec882751e7ac
Ronald Chen <ronald@battlefy.com>
2021-09-06 19:45:20 -0700

    reworked to repo the subfolder issue by inlining public into source and removing the routes workaround

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
+    src: { url: '/' }
   },
   plugins: [
     /* ... */
   ],
   routes: [
-    /* Enable an SPA Fallback in development: */
-    {"match": "routes", "src": "/subfolder", "dest": "/subfolder/index.html"},
+    /* This is the workaround */
+    // {"match": "routes", "src": "/subfolder", "dest": "/subfolder/index.html"},
   ],
   optimize: {
     /* Example: Bundle your final build: */
